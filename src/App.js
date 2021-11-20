9de5e1555b12c18b7f94bf73745fff6ac37083e1
Christian Ortiz <christian.ortiz2@globallogic.com>
2021-11-19 21:08:16 -0300

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      
         <p>
           Hola Mundo
         </p>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './NavBar.css'
-import logo from '../../logo_coderhouse.png';
+import logo from '../../assets/logo_coderhouse.png';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
 
 export default function NavBar() {
 
     return(
-        <nav>
-            <div className="container-logo">
-                <img src={logo} alt="Logo ecommerce"/>
-            </div>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>Products</a></li>
-                <li><a>Contact</a></li>
-                <li><a>About </a></li>
-            </ul>
-        </nav>
+        <AppBar position="static">
+            <nav>
+                <div className="container-logo">
+                    <img src={logo} alt="Logo ecommerce"/>
+                </div>
+                <ul>
+                    <li><Button variant="outlined">Home</Button></li>
+                    <li><Button variant="outlined">Products</Button></li>
+                    <li><Button variant="outlined">Contact</Button></li>
+                    <li><Button variant="outlined">About</Button></li>
+                </ul>   
+            </nav>
+        </AppBar>
     )
 }
