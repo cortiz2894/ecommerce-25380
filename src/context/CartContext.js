54822a73cb5c4887a5b6566a07fe21b7a5487181
Christian Daniel Ortiz Orobia <cortiz@sms-sudamerica.com>
2022-01-12 11:21:26 -0300

    feat: Add localstorage function

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,12 +3,17 @@ import { createContext, useState } from "react";
 const CartContext = createContext();
 
 const CartProvider = ({children}) => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState(JSON.parse(localStorage.getItem("productos")) || [])
     const [totalPrice, setTotalPrice] = useState(0)
 
     const addProducts = (product) => {
         setProducts([...products, product])
+        addProductStorage(product)
         setTotalPrice(totalPrice + product.price * product.quantity)
+    }
+
+    const addProductStorage = (product) => {
+        localStorage.setItem("productos", JSON.stringify([...products, product]))
     }
 
     const data = {
